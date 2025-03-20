a3999b64143019b4a40fd3b4de49922580dc2587
souzamariajulia <souzamariajulia44@gmail.com>
2025-03-19 22:04:32 -0300

    backend

--- backend/src/routes/todoRoutes.ts
+++ backend/src/routes/todoRoutes.ts
@@ -1,17 +1,34 @@
-import { Router } from "express";
-import { TodoModel } from "../models/Todo"; //importando o objeto criado em todo
-import { json } from "stream/consumers";
+import { Router, Request, Response } from "express";  
+import { TodoModel } from "../models/Todo";
 
-const router = Router()
+const router = Router();
 
-router.get("/", async (req, res) => {
+router.get("/", async (req: Request, res: Response) => {
   try {
-    const todos = await TodoModel.find()
-    res.json(todos)
+    const todos = await TodoModel.find(); 
+    res.json(todos);
   } catch (error) {
-    res.status(500).json({error: "Erro ao buscar dados"})
+    res.status(500).json({ error: "Erro ao buscar dados" });
   }
-})
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  const { title } = req.body;
+
+  if (!title) {
+    return res.status(400).json({ message: "Título é obrigatório" });
+  }
+  
+  const newTodo = new TodoModel({ title, completed: false });
+
+  try {
+    
+    await newTodo.save();
+
+    res.status(201).json(newTodo);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao salvar tarefa" });
+  }
+});
 
 export default router;
-
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,23 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import todoRoutes from "./routes/todoRoutes"; 
+import todoRoutes from "./routes/todoRoutes"; // Certifique-se de que o caminho está correto
 
-dotenv.config(); 
-mongoose.connect("mongodb://localhost:27017/todolist")
+dotenv.config();
+
+mongoose
+  .connect("mongodb://localhost:27017/todolist")
   .then(() => console.log("Conectado ao MongoDB"))
-  .catch(err => console.error("Erro ao conectar ao MongoDB:", err));
+  .catch((err) => console.error("Erro ao conectar ao MongoDB:", err));
+
 const app = express();
-app.use(express.json()); 
+app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
 
-app.use("/todos", todoRoutes); 
+app.use("/todos", todoRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
